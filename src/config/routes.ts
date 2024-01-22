debd0b941e0946b7e8612e6a653c4e7259fdba6e
FlavioJunior2021 <flavinhoj78@gmail.com>
2024-01-22 11:37:22 -0300

    Add Create product route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,9 @@
+import { createProductRoute } from "../routes/ProductRoute";
 import { authUserRoute, creatUserRoute } from "../routes/UserRoute";
 import { app } from "./fastify";
 
 export function registerRoutes() {
-	const routes = [creatUserRoute, authUserRoute];
+	const routes = [creatUserRoute, authUserRoute, createProductRoute];
 	routes.forEach((route) => {
 		app.register(route);
 	});
