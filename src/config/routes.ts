0f54e3051beffdc506707ecfe6f44f1d2d8f00de
FlavioJunior2021 <flavinhoj78@gmail.com>
2024-01-31 10:49:17 -0300

    Atualize routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,11 +1,11 @@
 import { createProductRoute, deleteProductRoute, getProductsByIdRoute, getProductsRoute, updateProductRoute } from "../routes/ProductRoute";
 import { registerSaleRoute } from "../routes/SaleRoute";
 import { createSupplierRoute, createproductSupplierRoute, deleteSupplierRoute, getProductsSupplierRoute, getSupplierRoute, updateSupplierRoute } from "../routes/SupplierRoute";
-import { authUserRoute, creatUserRoute } from "../routes/UserRoute";
+import { authUserRoute, creatUserRoute, deleteUserRoute, getUserRoute, updateUserRoute } from "../routes/UserRoute";
 import { app } from "./fastify";
 
 export function registerRoutes() {
-	const routes = [creatUserRoute, authUserRoute, createProductRoute, deleteProductRoute, updateProductRoute, getProductsRoute, getProductsByIdRoute, registerSaleRoute, createSupplierRoute, updateSupplierRoute, deleteSupplierRoute, getSupplierRoute, createproductSupplierRoute, getProductsSupplierRoute];
+	const routes = [creatUserRoute, authUserRoute, updateUserRoute, deleteUserRoute, getUserRoute, createProductRoute, deleteProductRoute, updateProductRoute, getProductsRoute, getProductsByIdRoute, registerSaleRoute, createSupplierRoute, updateSupplierRoute, deleteSupplierRoute, getSupplierRoute, createproductSupplierRoute, getProductsSupplierRoute];
 	routes.forEach((route) => {
 		app.register(route);
 	});
